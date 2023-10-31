5756832d91a542bbc3c5578960df73f4c65dc3ed
annie <annxanthopulo@gmail.com>
2023-10-30 21:46:46 -0300

    contrução do pagina

--- technicaltest-react/src/Principal.js
+++ technicaltest-react/src/Principal.js
@@ -2,7 +2,57 @@ import './input.css'
 
 function Principal() {
     return (
-    );
+        <div class="container mx-auto p-4">
+            <div class="grid grid-cols-5 gap-8 p-5 shadow-md">
+
+
+                <div  class="col-span-3 relative">
+                    <div  class="bg-image bg-cover bg-center h-full">
+                        <div class="absolute bottom-0 p-4 w-full">
+                            <h2 class="text-3xl text-white font-bold" >noticia principal</h2>
+                            <p class="text-sm text-white"> 10/10/10</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-span-2 grid grid-rows-4 gap-4">
+
+                    <div class=" h-32">
+                        <img src="/noticiaprincipal.jpg" class="w-1/2 h-full object-cover float-left"></img>
+                        <div class="p-4">
+                            <p class="text-sm">10/10/10</p>
+                            <h3 class="text-xl font-bold"> noticia 1</h3>
+                        </div>
+                    </div>
+                    <div class=" h-32">
+                        <img src="/noticiaprincipal.jpg" class="w-1/2 h-full object-cover float-left"></img>
+                        <div class="p-4">
+                            <p class="text-sm">10/10/10</p>
+                            <h3 class="text-xl font-bold"> noticia 1</h3>
+                        </div>
+                    </div>
+                    <div class=" h-32">
+                        <img src="/noticiaprincipal.jpg" class="w-1/2 h-full object-cover float-left"></img>
+                        <div class="p-4">
+                            <p class="text-sm">10/10/10</p>
+                            <h3 class="text-xl font-bold"> noticia 1</h3>
+                        </div>
+                    </div>
+                    <div class=" h-32">
+                        <img src="/noticiaprincipal.jpg" class="w-1/2 h-full object-cover float-left"></img>
+                        <div class="p-4">
+                            <p class="text-sm">10/10/10</p>
+                            <h3 class="text-xl font-bold"> noticia 1</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+);
 }
 
 export default Principal;
--- technicaltest-react/src/blade.js
+++ technicaltest-react/src/blade.js
@@ -4,22 +4,59 @@ function Blade() {
     return (
         <main class="container mx-auto px-4 py-6">
             <article class="bg-white rounded">
+                <div>
+                <p class="font-bold py-2">Mais lidas da semana</p>
                 <ul>
                     <li>
-                        <a>
-                            <div class="grid grid-cols-2 gap-4 shadow-md p-6 items-end">
-                                <div class="self-start">
-                                    <h2 class="text-3xl font-bold mb-2"> Título da Notícia</h2>
-                                    <p class="text-gray-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tortor a libero auctor commodo. Integer id lectus odio.</p>
-                                    <span class="text-gray-500 self-end" >Publicado em: 23/10/2021</span>
-                                </div>                                
-                                <div class="">
-                                    <img src="/noticiaprincipal.jpg" alt="Notícia" class="rounded"></img>
-                                </div>
+                        <div class="grid grid-cols-2 gap-4 shadow-md p-6 items-end">
+                            <div class="self-start">
+                                <h2 class="text-3xl font-bold mb-2"> Título da Notícia</h2>
+                                <p class="text-gray-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tortor a libero auctor commodo. Integer id lectus odio.</p>
+                                <span class="text-gray-500 self-end" >Publicado em: 23/10/2021</span>
+                            </div>                                
+                            <div class="">
+                                <img src="/noticiaprincipal.jpg" alt="Notícia" class="rounded"></img>
                             </div>
-                        </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="grid grid-cols-2 gap-4 shadow-md p-6 items-end">
+                            <div class="self-start">
+                                <h2 class="text-3xl font-bold mb-2"> Título da Notícia</h2>
+                                <p class="text-gray-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tortor a libero auctor commodo. Integer id lectus odio.</p>
+                                <span class="text-gray-500 self-end" >Publicado em: 23/10/2021</span>
+                            </div>                                
+                            <div class="">
+                                <img src="/noticiaprincipal.jpg" alt="Notícia" class="rounded"></img>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="grid grid-cols-2 gap-4 shadow-md p-6 items-end">
+                            <div class="self-start">
+                                <h2 class="text-3xl font-bold mb-2"> Título da Notícia</h2>
+                                <p class="text-gray-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tortor a libero auctor commodo. Integer id lectus odio.</p>
+                                <span class="text-gray-500 self-end" >Publicado em: 23/10/2021</span>
+                            </div>                                
+                            <div class="">
+                                <img src="/noticiaprincipal.jpg" alt="Notícia" class="rounded"></img>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="grid grid-cols-2 gap-4 shadow-md p-6 items-end">
+                            <div class="self-start">
+                                <h2 class="text-3xl font-bold mb-2"> Título da Notícia</h2>
+                                <p class="text-gray-600 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel tortor a libero auctor commodo. Integer id lectus odio.</p>
+                                <span class="text-gray-500 self-end" >Publicado em: 23/10/2021</span>
+                            </div>                                
+                            <div class="">
+                                <img src="/noticiaprincipal.jpg" alt="Notícia" class="rounded"></img>
+                            </div>
+                        </div>
                     </li>
                 </ul>
+                </div>
             </article>
         </main>
     );
